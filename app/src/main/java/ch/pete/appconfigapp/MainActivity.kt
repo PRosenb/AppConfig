88229f356d01859327636c8555335809be914ce2
Peter Rosenberg <p.rosenberg@gmx.net>
2020-05-10 19:41:11 +0200

    add up navigation

--- app/src/main/java/ch/pete/appconfigapp/MainActivity.kt
+++ app/src/main/java/ch/pete/appconfigapp/MainActivity.kt
@@ -2,12 +2,13 @@ package ch.pete.appconfigapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.ViewModelProvider
 import ch.pete.appconfigapp.configdetail.ConfigDetailFragment
 import timber.log.Timber
 
 
-class MainActivity : AppCompatActivity(), MainView {
+class MainActivity : AppCompatActivity(), MainView, FragmentManager.OnBackStackChangedListener {
     private lateinit var viewModel: AppConfigViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,7 +20,19 @@ class MainActivity : AppCompatActivity(), MainView {
 
         viewModel = ViewModelProvider(this).get(AppConfigViewModel::class.java)
         viewModel.mainView = this
-//        viewModel.initViewModel()
+
+        supportFragmentManager.addOnBackStackChangedListener(this)
+        shouldDisplayHomeUp()
+    }
+
+    override fun onBackStackChanged() {
+        shouldDisplayHomeUp()
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        //This method is called when the up button is pressed. Just the pop back stack.
+        supportFragmentManager.popBackStack()
+        return true
     }
 
     override fun showDetails(configId: Long) {
@@ -37,5 +50,11 @@ class MainActivity : AppCompatActivity(), MainView {
 
         fragmentTransaction.addToBackStack(null)
         fragmentTransaction.commit()
+    }
+
+    private fun shouldDisplayHomeUp() {
+        // Enable Up button only if there are entries in the back stack
+        val canGoBack = supportFragmentManager.backStackEntryCount > 0
+        supportActionBar?.setDisplayHomeAsUpEnabled(canGoBack)
     }
 }
