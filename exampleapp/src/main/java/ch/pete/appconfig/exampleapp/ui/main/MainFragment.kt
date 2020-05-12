8f9da1a7af7e08e2a3fd76272c7c15909c500b84
Peter Rosenberg <p.rosenberg@gmx.net>
2020-05-12 22:23:32 +0200

    add example app

---
+++ exampleapp/src/main/java/ch/pete/appconfig/exampleapp/ui/main/MainFragment.kt
@@ -0,0 +1,28 @@
+package ch.pete.appconfig.exampleapp.ui.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import ch.pete.appconfig.exampleapp.R
+
+class MainFragment : Fragment() {
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private val viewModel: MainViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+}
---
+++ exampleapp/src/main/java/ch/pete/appconfig/exampleapp/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package ch.pete.appconfig.exampleapp.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
