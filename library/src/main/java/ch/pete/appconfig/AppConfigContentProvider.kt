92d7319cca521bbaf282e5ab5dfaced7ddc4b405
Peter Rosenberg <p.rosenberg@gmx.net>
2020-05-15 22:52:20 +0200

    simplify content provider

--- library/src/main/java/ch/pete/appconfig/AppConfigContentProvider.kt
+++ library/src/main/java/ch/pete/appconfig/AppConfigContentProvider.kt
@@ -2,6 +2,7 @@ package ch.pete.appconfig
 
 import android.content.ContentProvider
 import android.content.ContentValues
+import android.content.Context
 import android.database.Cursor
 import android.net.Uri
 import android.os.Binder
@@ -22,21 +23,7 @@ class AppConfigContentProvider : ContentProvider() {
     ): Int {
         Timber.d("uri: $uri, values: $values")
         context?.let { context ->
-            val callingApplicationId = context.packageManager.getNameForUid(Binder.getCallingUid())
-            val callingSignature = SignatureUtils.getSignatureHash(context, callingApplicationId)
-            Timber.d("callingApp: $callingApplicationId, callingAppSignature: $callingSignature")
-
-            val allowedApp = AppConfig.authorizedApps.firstOrNull {
-                callingApplicationId == it.applicationId
-                        && callingSignature == it.signature
-            }
-
-            allowedApp ?: throw SecurityException("Access Denied")
-            if (values?.containsKey("TEST_ACCESS_DENIED") == true) {
-                throw SecurityException("Access Denied")
-            } else {
-                Timber.d("authorized access to $allowedApp")
-            }
+            checkIfAllowed(context, values)
 
             var appliedKeysCount = 0
             values?.let { values ->
@@ -47,31 +34,63 @@ class AppConfigContentProvider : ContentProvider() {
                 }
 
                 if (AppConfig.storeValuesToSharedPreferences) {
-                    val prefs = PreferenceManager.getDefaultSharedPreferences(context)
-                    val editor = prefs.edit()
-                    values.keySet()
-                        .filter {
-                            if (AppConfig.authorizedKeys.isEmpty()) {
-                                true // allow all if authorizedKeys is empty
-                            } else {
-                                AppConfig.authorizedKeys.contains(it)
-                            }
-                        }
-                        .forEach { key ->
-                            val value = values.get(key)
-                            when (val dataType = value.javaClass) {
-                                String::class.java -> editor.putString(key, value as String)
-                                Boolean::class.java -> editor.putBoolean(key, value as Boolean)
-                                else -> throw IllegalArgumentException("Unsupported data type $dataType")
-                            }
-                            appliedKeysCount++
-                        }
-                    editor.apply()
+                    appliedKeysCount = storeValuesToSharedPreferences(context, values)
                 }
             }
 
             return appliedKeysCount
         } ?: throw IllegalArgumentException("context is null")
+    }
+
+    private fun storeValuesToSharedPreferences(context: Context, values: ContentValues): Int {
+        var appliedKeysCount = 0
+        val prefs = PreferenceManager.getDefaultSharedPreferences(context)
+        val editor = prefs.edit()
+        values.keySet()
+            .filter {
+                if (AppConfig.authorizedKeys.isEmpty()) {
+                    true // allow all if authorizedKeys is empty
+                } else {
+                    AppConfig.authorizedKeys.contains(it)
+                }
+            }
+            .forEach { key ->
+                val value = values.get(key)
+                when (val dataType = value.javaClass) {
+                    String::class.java -> editor.putString(key, value as String)
+                    Boolean::class.java -> editor.putBoolean(key, value as Boolean)
+                    else -> throw IllegalArgumentException("Unsupported data type $dataType")
+                }
+                appliedKeysCount++
+            }
+        editor.apply()
+        return appliedKeysCount
+    }
+
+    @Throws(SecurityException::class)
+    private fun checkIfAllowed(context: Context, values: ContentValues?) {
+        val callingApplicationId = context.packageManager.getNameForUid(Binder.getCallingUid())
+        if (callingApplicationId == null) {
+            Timber.e("callingApplicationId is null")
+        }
+
+        val allowedApp = callingApplicationId?.let {
+            val callingSignature =
+                SignatureUtils.getSignatureHash(context, callingApplicationId)
+            Timber.d("callingApp: $callingApplicationId, callingAppSignature: $callingSignature")
+
+            AppConfig.authorizedApps.firstOrNull {
+                callingApplicationId == it.applicationId
+                        && callingSignature == it.signature
+            }
+        }
+
+        allowedApp ?: throw SecurityException("Access Denied")
+        if (values?.containsKey("TEST_ACCESS_DENIED") == true) {
+            throw SecurityException("Access Denied")
+        } else {
+            Timber.d("authorized access to $allowedApp")
+        }
     }
 
     override fun getType(uri: Uri): String? = ""
