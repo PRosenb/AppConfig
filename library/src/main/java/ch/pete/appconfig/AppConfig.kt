b3a0d9cabe119bd2d3b2c46f78ac78e918696c40
Peter Rosenberg <p.rosenberg@gmx.net>
2020-05-16 08:44:32 +0200

    adjust authorizedApps to new key

--- library/src/main/java/ch/pete/appconfig/AppConfig.kt
+++ library/src/main/java/ch/pete/appconfig/AppConfig.kt
@@ -35,14 +35,7 @@ object AppConfig {
         authorizedApps.add(
             AuthorizedApp(
                 applicationId = "ch.pete.appconfigapp",
-                signature = "06:02:0D:0D:0E:0D:05:07:0A:00:0E:03:0D:06:03:00:02:0B:06:07:07:02:03:06:0E:0B:08:07:08:0B:07:01"
-            )
-        )
-        @Suppress("MaxLineLength")
-        authorizedApps.add(
-            AuthorizedApp(
-                applicationId = "ch.pete.appconfigapp",
-                signature = "0F:00:04:0F:09:08:09:01:00:06:02:0C:03:07:05:03:00:01:04:09:03:05:02:06:0B:01:00:00:05:0E:0F:02"
+                signature = "07:0D:00:06:0D:0F:02:0D:0C:0C:05:04:05:00:03:07:00:0A:0F:0D:07:0E:0B:01:01:0C:0F:0B:00:07:09:0C"
             )
         )
     }
